654c5ef33d2f2565f68de062d48c72074b233fd3
Paul Cunningham <pjcunningham@borsuk.co.uk>
2024-02-11 11:26:09 +0000

    Update requirements

--- setup.py
+++ setup.py
@@ -240,8 +240,8 @@ setup_dict = dict(
     ],
     packages=find_packages(exclude=(TESTS_DIRECTORY,)),
     install_requires=[
-        'PyMySQL==0.6.7',
-        'mysql-replication>=0.8',
+        'PyMySQL>=1.0.1',
+        'mysql-replication>=1.0.2',
         'blinker',
     ] + python_version_specific_requires,
     # Allow tests to be run with `python setup.py test'.
